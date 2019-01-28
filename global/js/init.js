2a9322cb7a6048a82b95e0d863fd8648a8780867
Iain.Wood <isw44@open.ac.uk>
2019-01-28 17:12:47 +0000

    google drive

--- global/js/init.js
+++ global/js/init.js
@@ -209,12 +209,9 @@ $(document).on("keydown", disableF5);
 */
 var changes = true;        
 window.onbeforeunload = function() {
-    if (changes)
-    {
-        var message = "Are you sure you want to navigate away from this page?\n\nYou have started writing or editing a post.\n\nPress OK to continue or Cancel to stay on the current page.";
-        if (confirm(message)) return true;
-        else return false;
-    }
+  //swal("Hmmm", 'Saving "just happens" with wavemaker!', "success");;
+  return false;
+ 
 }
 
 
