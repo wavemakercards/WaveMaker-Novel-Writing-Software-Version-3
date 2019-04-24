e3e9c8e4cb73519f4567944dbd9638d4eb0fe547
Iain Wood <mayasky76@gmail.com>
2019-04-24 10:35:38 +0100

    added save the cat template

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /*
 Mod version number to force system wide update
 */
-var version = "?=_" + "3.0.1.8";
+var version = "?=_" + "3.0.1.9";
 console.log("Version ", version);
 versionTrigger = "?=_" + version;
 'use strict';
